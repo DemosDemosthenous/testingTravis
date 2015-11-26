02c6769370c82924c2b4ac67b78c8dd969ce3f09
DemosDemosthenous <demos11@hotmail.com>
2015-11-26 01:36:45 +0000

    Updated Class Diagram.dia and added a new class Player.java

--- src/main/java/monopoly/Player.java
+++ src/main/java/monopoly/Player.java
@@ -1,5 +1,12 @@
 package monopoly;
 
-public class Player {
+import java.util.List;
 
+public class Player {
+public String name;
+public double money;
+public int position;
+public Token token;
+public List<Property>properties;
+	
 }
